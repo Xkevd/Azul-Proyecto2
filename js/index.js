ed6d5e5bf9f13dc5ed1a72197fbf2b9edab2dcd3
Kevin del Hoyo <kevinalex11@hotmail.com>
2026-02-14 15:08:33 -0300

    responsive

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Lógica simple para el acordeón de FAQ
-document.querySelectorAll('.faq-quest button').forEach(btn => {
-    btn.addEventListener('click', () => {
-        const answer = btn.nextElementSibling;
+document.querySelectorAll('.faq-quest').forEach(item => {
+    item.addEventListener('click', () => {
+        const answer = item.querySelector('.faq-ans');
         answer.classList.toggle('activo');
     });
 });
